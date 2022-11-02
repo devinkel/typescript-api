20abb985d16f01435053b4e95f4afa6b1ad6beaf
kelvin ramon <kelvinkelrosa@gmail.com>
2022-11-02 17:35:34 -0300

    criado setup da app, criado primeira rota, como compor tipos que ja existe

--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -1,11 +1,11 @@
 import * as path from 'path';
-import moduleAlias from 'module-alias';
+import moduleAlias, { addAliases } from 'module-alias';
 
 // buscando o root do diretorio atual
 const files = path.resolve(__dirname, '../..');
 
 // adicionando ALIAS para os caminhos de src e test, para funcionarem a partir de @
 moduleAlias.addAliases({
-    '@scr' : path.join(files, 'src'),
+    '@src' : path.join(files, 'src'),
     '@test': path.join(files, 'test')
 });
--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -2,10 +2,10 @@ import supertest from "supertest";
 
 describe('Beach Forecast functional tests', () => {
     it(' should return a forecast with just a few times ',async () => {
-        const {body , status} = await supertest(app).get('/forecast');
+        const {body , status} = await global.testRequest.get('/forecast');
         expect(status).toBe(200);
-        expect(body).toBe([{
-            '': ''
+        expect(body).toEqual([{
+            "k": "kelvin"
         }])
     })
 });
